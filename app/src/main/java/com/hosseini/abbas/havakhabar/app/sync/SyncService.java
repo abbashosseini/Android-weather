f3da6c34ab0f3e4d7811a8cae864de9e578a17ec
abbashosseini <abbashosseini@mail.com>
2016-01-19 11:12:07 +0330

    Fix Bugs

---
+++ app/src/main/java/com/hosseini/abbas/havakhabar/app/sync/SyncService.java
@@ -0,0 +1,30 @@
+package com.hosseini.abbas.havakhabar.app.sync;
+
+/**
+ * Created by paulshi on 10/31/14.
+ */
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SyncService", "onCreate - SyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
